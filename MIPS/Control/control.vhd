1dd965f99bb110188aec205cba07006ae892a043
wmhudson <wmhudson@co2050-06.ece.iastate.edu>
2022-10-12 16:11:16 -0500

    Working control unit with tb
    - Just waiting on ALUOp spec

--- MIPS/Control/control.vhd
+++ MIPS/Control/control.vhd
@@ -3,51 +3,49 @@ use IEEE.std_logic_1164.all;
 
 entity control is 
 	port(	i_OP	: in std_logic_vector(5 downto 0);
+		i_FUNCT	: in std_logic_vector(5 downto 0);
 		RegDst	: out std_logic;
 		Jump	: out std_logic;
 		Branch	: out std_logic;
-		MemRead	: out std_logic;
-		MemtoReg	: out std_logic;
+		Reg31	: out std_logic;
+		MemRead	: out std_logic;	
+		MemtoReg	: out std_logic;	
 		ALUOp	: out std_logic_vector(3 downto 0);
-		MemWrite	: out std_logic;
+		MemWrite	: out std_logic;	
 		ALUSrc	: out std_logic;
-		RegWrite	: out std_logic;
+		RegWrite	: out std_logic
 );
 end control;
 
-architecture structure of AddSub is
+architecture behavioral of control is
 
-component FullAdder is
-	generic(N	: integer	:= 32);
-	port(	i_A	: in std_logic_vector(N-1 downto 0);
-		i_B	: in std_logic_vector(N-1 downto 0);
-		i_C	: in std_logic;
-		o_S	: out std_logic_vector(N-1 downto 0);
-		o_C	: out std_logic);
-end component;
-
-component xorg2 is
-	port(	i_A	: in std_logic;
-		i_B	: in std_logic;
-		o_F	: out std_logic);
-end component;
-
-signal s_B	: std_logic_vector(31 downto 0);
+signal top2	: std_logic_vector(1 downto 0);
+signal top4	: std_logic_vector(3 downto 0);
+signal bot3	: std_logic_vector(2 downto 0);
+signal top5	: std_logic_vector(4 downto 0);
+signal noOp	: std_logic;
+signal jr	: std_logic;
+signal MemReadS	: std_logic;
 
 begin
 
-g_FullAdder: FullAdder port map(
-		i_A	=> i_A,
-		i_B	=> s_B,
-		i_C	=> i_ADDSUB,
-		o_S	=> o_S,
-		o_C	=> o_C);
+top2	 <= i_OP(5 downto 4);
+top4	 <= i_OP(5 downto 2);
+bot3 	 <= i_OP(2 downto 0);
+top5	 <= i_OP(5 downto 1);
+noOp 	 <= '1' when i_OP = "000000" else '0';
+jr 	 <= '1' when noOp = '1' and i_FUNCT = "001000" else '0';
+MemReadS <= '1' when (i_OP = "100011") else '0';
 
-g_B: for i in 0 to 31 generate
-	xori: xorg2 port map (
-		i_A	=> i_ADDSUB,
-		i_B	=> i_B(i),
-		o_F	=> s_B(i));
-end generate;
+RegDst	 <= '1' when (noOp = '1' or (i_OP = "011111" and i_FUNCT = "010010")) else '0';
+Jump	 <= '1' when (top5 = "00001" or (noOp = '1' and jr = '1')) else '0';
+Branch	 <= '1' when (top5 = "00010") else '0';
+Reg31	 <= '1' when (i_OP = "000011") else '0';
+MemRead	 <= MemReadS;
+MemtoReg <= MemReadS;
+ALUOp	 <= "0000"; --temp
+MemWrite <= '1' when (i_OP = "101011") else '0';
+ALUSrc	 <= '1' when (top5 = "00001" or top5 = "00010" or top5 = "00100" or i_OP = "001010" or top4 = "0011" or (noOp = '1' and jr = '1') or (top2 = "10" and bot3 = "011")) else '0';
+RegWrite <= '0' when (i_OP = "101011" or top5 = "00010" or i_OP = "000010" or (noOp = '1' and jr = '1')) else '1'; 
 
-end structure;
+end behavioral;
---
+++ MIPS/Control/tb_control.vhd
@@ -0,0 +1,209 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb_control is
+  generic(gCLK_HPER   : time := 50 ns);
+end tb_control;
+
+architecture behavior of tb_control is
+  
+  -- Calculate the clock period as twice the half-period
+  constant cCLK_PER  : time := gCLK_HPER * 2;
+
+
+  component control
+    port(	i_OP	: in std_logic_vector(5 downto 0);
+		i_FUNCT	: in std_logic_vector(5 downto 0);
+		RegDst	: out std_logic;
+		Jump	: out std_logic;
+		Branch	: out std_logic;
+		Reg31	: out std_logic;
+		MemRead	: out std_logic;
+		MemtoReg: out std_logic;
+		ALUOp	: out std_logic_vector(3 downto 0);
+		MemWrite: out std_logic;
+		ALUSrc	: out std_logic;
+		RegWrite: out std_logic);
+  end component;
+
+  -- Temporary signals to connect to the dff component.
+  signal s_CLK, s_RST : std_logic;
+  signal s_RegDst, s_Jump, s_Branch, s_Reg31, s_MemRead, s_MemtoReg, s_MemWrite, s_ALUSrc, s_RegWrite : std_logic;
+  signal s_OP, s_FUNCT : std_logic_vector(5 downto 0);
+  signal s_ALUOp : std_logic_vector(3 downto 0);
+
+begin
+
+  DUT: control 
+  port map(	i_OP	=> s_OP,
+		i_FUNCT	=> s_FUNCT,
+		RegDst	=> s_RegDst,
+		Jump	=> s_Jump,
+		Branch	=> s_Branch,
+		Reg31	=> s_Reg31,
+		MemRead	=> s_MemRead,
+		MemtoReg=> s_MemtoReg,
+		ALUOp	=> s_ALUOp,
+		MemWrite=> s_MemWrite,
+		ALUSrc	=> s_ALUSrc,
+		RegWrite=> s_RegWrite);
+
+  -- This process sets the clock value (low for gCLK_HPER, then high
+  -- for gCLK_HPER). Absent a "wait" command, processes restart 
+  -- at the beginning once they have reached the final statement.
+  P_CLK: process
+  begin
+    s_CLK <= '0';
+    wait for gCLK_HPER;
+    s_CLK <= '1';
+    wait for gCLK_HPER;
+  end process;
+  
+-- Testbench process  
+  P_TB: process
+  begin
+    s_RST <= '1';
+    wait for gCLK_HPER / 2;
+    s_RST <= '0';
+
+-- add
+	s_OP		<= "000000";
+	s_FUNCT		<= "100000";
+	wait for cCLK_PER;
+
+-- addi
+	s_OP		<= "001000";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- addiu
+	s_OP		<= "001001";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- addu
+	s_OP		<= "000000";
+	s_FUNCT		<= "100001";
+	wait for cCLK_PER;
+    
+-- and
+	s_OP		<= "000000";
+	s_FUNCT		<= "100100";
+	wait for cCLK_PER;
+    
+-- andi
+	s_OP		<= "001100";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- lui
+	s_OP		<= "001111";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- lw
+	s_OP		<= "100011";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- nor
+	s_OP		<= "000000";
+	s_FUNCT		<= "100111";
+	wait for cCLK_PER;
+    
+-- xor
+	s_OP		<= "000000";
+	s_FUNCT		<= "100110";
+	wait for cCLK_PER;
+    
+-- xori
+	s_OP		<= "001110";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- or
+	s_OP		<= "000000";
+	s_FUNCT		<= "100101";
+	wait for cCLK_PER;
+    
+-- ori
+	s_OP		<= "001101";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- slt
+	s_OP		<= "000000";
+	s_FUNCT		<= "101010";
+	wait for cCLK_PER;
+    
+-- slti
+	s_OP		<= "001010";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- sll
+	s_OP		<= "000000";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- srl
+	s_OP		<= "000000";
+	s_FUNCT		<= "000010";
+	wait for cCLK_PER;
+    
+-- sra
+	s_OP		<= "000000";
+	s_FUNCT		<= "000011";
+	wait for cCLK_PER;
+    
+-- sw
+	s_OP		<= "101011";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- sub
+	s_OP		<= "000000";
+	s_FUNCT		<= "100010";
+	wait for cCLK_PER;
+    
+-- beq
+	s_OP		<= "000100";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- bne
+	s_OP		<= "000101";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- j
+	s_OP		<= "000010";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- jal
+	s_OP		<= "000011";
+	s_FUNCT		<= "000000";
+	wait for cCLK_PER;
+    
+-- jr
+	s_OP		<= "000000";
+	s_FUNCT		<= "001000";
+	wait for cCLK_PER;
+    
+-- repl.qb
+	s_OP		<= "011111";
+	s_FUNCT		<= "010010";
+	wait for cCLK_PER;
+    
+-- movn
+	s_OP		<= "000000";
+	s_FUNCT		<= "001011";
+	wait for cCLK_PER;
+    
+-- RESET
+    s_RST <= '1';
+    wait;
+  end process;
+  
+end behavior;
