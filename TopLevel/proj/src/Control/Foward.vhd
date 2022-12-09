8fab96241a388b467cac28a7bd7ab60008a54888
saguilar <saguilar@co1313-08.ece.iastate.edu>
2022-12-09 15:40:05 -0600

    changes to Foward did not save the first time

--- TopLevel/proj/src/Control/Foward.vhd
+++ TopLevel/proj/src/Control/Foward.vhd
@@ -7,8 +7,7 @@ entity Foward is
 		i_ExMem	: in std_logic_vector(5 downto 0);
 		i_ExALU	: in std_logic_vector(5 downto 0);
 		o_RS	: out std_logic_vector(1 downto 0);
-		o_RT	: out std_logic_vector(1 downto 0);
-);
+		o_RT	: out std_logic_vector(1 downto 0));
 end Foward;
 
 architecture behavioral of Foward is
@@ -20,14 +19,14 @@ signal RT	: std_logic_vector(1 downto 0);
 
 begin
 
-Rs <= '0' when(i_ExALU = 0 and i_ExMem = 0) else
-      '1' when(i_RS = i_ExALU) else
-      '2' when(i_RS = i_ExMem) else '0';
+Rs <= "00" when(i_ExALU = "00000" and i_ExMem = "00000") else
+      "01" when(i_RS = i_ExALU) else
+      "10" when(i_RS = i_ExMem) else "00";
 
 
-RT <= '0' when(i_ExALU = 0 and i_ExMem = 0) else 
-      '1' when(i_RT = i_ExALU) else
-      '2' when(i_RT = i_ExMem) else '0';
+RT <= "00" when(i_ExALU = "00000" and i_ExMem = "00000") else 
+      "01" when(i_RT = i_ExALU) else
+      "10" when(i_RT = i_ExMem) else "00";
 
 o_RS <= Rs;
 o_RT <= RT;
